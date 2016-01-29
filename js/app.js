1d3bde36a5b48bcae7ffbf249098931f32fbed7d
KowalskiAlexander <tsli.alex@outlook.com>
2016-01-29 14:44:06 +0800

    Set up the programming framework.
    GO GO GO!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-// Enemies our player must avoid
+// declare variables
+
+var canvas_width = 10;
+var canvas_height = 20;
+
+
+// Enemies our player must avoidw
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -24,11 +30,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images\char-boy.png';
+}
+
+Player.prototype.update = function () {
+    // todo
+};
+
+Player.prototype.render = function () {
+  // todo
+};
+
+Player.prototype.handleInput = function () {
+  // todo
+};
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var player = new Player();
 
 
 
