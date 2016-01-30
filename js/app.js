be54dfb68957a58e8ea72524ed2f5dd46ffd7d18
KowalskiAlexander <tsli.alex@outlook.com>
2016-01-30 22:51:11 +0800

    First prototype.

--- js/app.js
+++ js/app.js
@@ -14,8 +14,10 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 
     // the initial position and speed of any enemy
-    this.x = ;
-    var _speed = 30;// todo
+    this.x = 0;
+    this.y = Math.random() * 184 + 50; // todo
+
+    this._speed = Math.random() * 256; // todo
 };
 
 // Update the enemy's position, required method for game
@@ -25,9 +27,13 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-
     // update the enemy. Make the enemy move.
-    this.x += dt * _speed;
+    this.x += dt * this._speed;
+
+    // renew the enemy
+    if (this.x >= canvas_width){
+      this.x = 0;
+    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -39,26 +45,53 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-  this.sprite = 'images\char-boy.png';
-  this._speed = 30; // todo
+  this.sprite = 'images/char-boy.png';
+  // initial speed of the player
+  this._speed = 50;
+  // initial position of the player
+  this.x = 202.5;
+  this.y = 383;
 
-  // the initial position of the Player
-  // todo
 }
 
-Player.prototype.update = function () {
-    // todo
-    this.x = ;
-    this.y = ;
+Player.prototype.update = function() {
+  // todo
+  // Prevent the player go off the canvas.
+  if (this.y > 383 ) {
+      this.y = 383;
+  }
+  if (this.x > 402.5) {
+      this.x = 402.5;
+  }
+  if (this.x < 2.5) {
+      this.x = 2.5;
+  };
 };
 
 Player.prototype.render = function () {
-  // todo
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function () {
+Player.prototype.handleInput = function(key) {
   // todo
+  switch (key) {
+    case 'left':
+      this.x -= this._speed;
+      break;
+    case 'right':
+      this.x += this._speed;
+      break;
+    case 'up':
+      this.y -= this._speed;
+      break;
+    case 'down':
+      this.y += this._speed;
+      break;
+    // todo
+    default:
+      this.x = this.x;
+      this.y = this.y;
+  };
 };
 
 
@@ -68,7 +101,9 @@ Player.prototype.handleInput = function () {
 // Place the player object in a variable called player
 var allEnemies = [];
 var player = new Player();
+var enemy = new Enemy();
 
+allEnemies.push(enemy);
 
 
 // This listens for key presses and sends the keys to your
